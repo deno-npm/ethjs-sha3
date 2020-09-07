ffb8795144764cfd471e7bfdac7ac4b979d8477f
Nicolas Guerrero <stephenguerrero43@gmail.com>
2020-09-06 20:04:30 -0500

    0.1.3

---
+++ README.md
@@ -0,0 +1,3 @@
+This library is deprecated, but required as a legacy library for Web3
+
+Based on the original work by [silentcicero](https://github.com/silentcicero)
---
+++ deps.js
@@ -0,0 +1,4 @@
+export { default as BigNumber } from "https://cdn.skypack.dev/bignumber.js@9.0.0";
+export { default as Buffer } from "https://cdn.skypack.dev/buffer@5.6.0";
+export { default as jssha3 } from "https://cdn.skypack.dev/js-sha3@0.5.5";
+export * as testing from "https://deno.land/std@0.68.0/testing/asserts.ts";
